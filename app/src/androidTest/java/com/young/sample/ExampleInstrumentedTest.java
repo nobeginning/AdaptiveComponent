e24a29e85d41878bb5fac51b084285f2bbdd8c3e
xucun <xucun.yang@nebulas.io>
2020-11-27 13:10:48 +0800

    fix error

--- app/src/androidTest/java/com/young/sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/young/sample/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package com.young.sample;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//
         assertEquals("com.young.adaptive", appContext.getPackageName());
     }
 }
